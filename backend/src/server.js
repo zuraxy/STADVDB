e92991ba16517e0942592760be895f4a205740f6
Zuraxy <ZuraxyOfficial@gmail.com>
2025-11-24 14:51:11 +0800

    setup: MCO2

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const dotenv = require('dotenv');
-
-
-require('dotenv').config({ path: '../../.env' });
-
-const PORT =  process.env.PORT || 3000
-
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
